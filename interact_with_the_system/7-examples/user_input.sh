3941ab90ff4f0f3f42ff4e9d0ee73c7bb82af0d1
hrvthgergo <hrvth.gergo@outlook.com>
2024-02-05 23:24:58 +0100

    add command shift and how to use with positional params to the examples and the description files

--- interact_with_the_system/7-examples/user_input.sh
+++ interact_with_the_system/7-examples/user_input.sh
@@ -5,10 +5,12 @@
 #3) Last Modified: 20240131
 
 #4) Description: collecting and printing user inputs
-#5) Usage: ./unser_input.sh param1 param2 param3
+#5) Usage: ./user_input.sh param1 param2 param3
 
 echo "the script name is: $0, and the number of command line args is: $#."
 echo "this is the first positional parameter -> $1"
+shift
+echo "this is the second positional parameter -> $1"
 
 read -p "please type a 4 digit long number within 10 seconds:" -n 4 -t 10 -s four_digit_num
 echo ${four_digit_num}
@@ -19,6 +21,7 @@ select option in option1 option2 option3;do
     break
 done
 
-echo "this is the third positional parameter -> $3"
+# i should use $2 because i shifted $1 in the 12th row.
+echo "this is the third positional parameter -> $2"
 
 exit 0
