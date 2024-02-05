f865eb6e5d01ee12abcee974d499ed39095c56b5
hrvthgergo <hrvth.gergo@outlook.com>
2024-02-06 00:01:14 +0100

    add bash built in param vars to echo

--- interact_with_the_system/7-examples/user_input.sh
+++ interact_with_the_system/7-examples/user_input.sh
@@ -8,8 +8,10 @@
 #5) Usage: ./user_input.sh param1 param2 param3
 
 echo "the script name is: $0, and the number of command line args is: $#."
+echo "arguments are: $@"
 echo "this is the first positional parameter -> $1"
 shift
+echo "the number of command line args after shift: $#."
 echo "this is the second positional parameter -> $1"
 
 read -p "please type a 4 digit long number within 10 seconds:" -n 4 -t 10 -s four_digit_num
@@ -23,5 +25,7 @@ done
 
 # i should use $2 because i shifted $1 in the 12th row.
 echo "this is the third positional parameter -> $2"
+echo "the process id: $$"
+echo "the last exit code: $?"
 
 exit 0
